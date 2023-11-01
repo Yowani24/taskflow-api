b897901b242eb0e95d135a18093f41eba3f7e010
joao da costa <yowani24@outlook.com>
2023-11-01 14:22:18 -0300

    Feat: new url connection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'https://taskfllow.netlify.app',
+    ],
     credentials: true,
   });
 
